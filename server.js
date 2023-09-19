088a7e45bdff13209aaeb99532fa1e5b8b53fa3d
gbdev83 <bleingauthier@gmail.com>
2023-09-19 10:09:28 +0200

    svgo installed

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(express.json());
 const path = require("path");
 
 
-/*if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
-}*/
+}
 
 app.get("/", (req, res) => {
   res.send("Welcome to eShop website.");
